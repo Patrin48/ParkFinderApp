e087d7a963ebd5367bce59fe556e8aa8090e9037
DESKTOP-E636J9N\Дмитрий <merenkov_dmitry@mail.ru>
2017-10-09 12:47:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/parkfinder/MainFragment.java
@@ -0,0 +1,41 @@
+package com.example.parkfinder;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+
+         FloatingActionButton fab = (FloatingActionButton) rootview.findViewById(R.id.fab);
+         fab.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                         .setAction("Action", null).show();
+             }
+          });
+return rootview;
+    }
+
+}
