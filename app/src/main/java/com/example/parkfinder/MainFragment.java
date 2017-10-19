c9a5e6ada093708b199a890500136371fd881857
Patrin Dmitry <patrin48rus@ya.ru>
2017-10-19 16:32:38 +0300

    19-10-17

--- app/src/main/java/com/example/parkfinder/MainFragment.java
+++ app/src/main/java/com/example/parkfinder/MainFragment.java
@@ -11,12 +11,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 
 public class MainFragment extends Fragment {
 
+    private SupportMapFragment mSupportMapFragment;
 
     public MainFragment() {
         // Required empty public constructor
@@ -26,10 +36,39 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_build_way, container, false);
+        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.map, mSupportMapFragment).commit();
+        }
+
+        if (mSupportMapFragment != null) {
+            mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    if (googleMap != null) {
+
+                        googleMap.getUiSettings().setAllGesturesEnabled(true);
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(52.601818, 39.5047206))
+                                .title("Your car's here!"));
+                        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(52.601818, 39.5047206)).zoom(14.0f).build();
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+                        googleMap.moveCamera(cameraUpdate);
+
+                    }
+
+                }
+            });
+
+
+        }
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
-
 }
+
+
 
