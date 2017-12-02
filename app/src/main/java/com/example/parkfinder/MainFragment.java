7ff9c888f099f6b843df75328780ba672fd1bde7
Alexey Gridnev <marchgridnev96@gmail.com>
2017-12-02 17:40:35 +0300

    PlacesList FROM API

--- app/src/main/java/com/example/parkfinder/MainFragment.java
+++ app/src/main/java/com/example/parkfinder/MainFragment.java
@@ -1,6 +1,9 @@
 package com.example.parkfinder;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 import com.google.android.gms.maps.CameraUpdate;
@@ -25,6 +29,16 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,10 +58,12 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View v =  inflater.inflate(R.layout.fragment_main, container, false);
         fabToolbar = ((FABToolbarLayout) v.findViewById(R.id.fabtoolbar));
         mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mSupportMapFragment == null) {
+
             FragmentManager fragmentManager = getFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             mSupportMapFragment = SupportMapFragment.newInstance();
@@ -81,10 +97,6 @@ public class MainFragment extends Fragment {
         ImageView im2 = (ImageView) v.findViewById(R.id.two);
         ImageView im3 = (ImageView) v.findViewById(R.id.three);
 
-
-
-
-
         im1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -115,6 +127,7 @@ public class MainFragment extends Fragment {
         fabToolbar.show();
             }
         });
+
         return v;
     }
 }
